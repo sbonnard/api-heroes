493cdb926e0a1e64fc50c12acac424b58f81659b
Guillaume Belleuvre <guillaumebelleuvre@gmail.com>
2024-04-10 15:42:25 +0200

    use already created functions and getAttackScore function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+
+/**
+ * Return a random value between 0 and a chosen number.
+ * @param {number} max - chosen number
+ * @returns {number} - random value
+ */
+function getRandomValue(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
+
+/**
+ * Gets a random value from an array
+ * @param {array} array of datas 
+ * @returns {*} - a random value
+ */
+function getRandomArrayValue(array) {
+    return array[getRandomValue(array.length - 1)];
+}
+
 const characters = [
     {
         name: 'Godzilla',
@@ -33,4 +53,21 @@ const characters = [
     }
 ];
 
+/**
+ * Get random attack score from character stats
+ * @param {object} attacker - An object representing a character
+ * @returns {number} - Random attack score
+ */
+function getAttackScore(attacker) {
+    return getRandomValue(attacker.weapon) + attacker.xp;
+}
+
 console.table(characters);
+
+const attacker = getRandomArrayValue(characters);
+// const defender;
+
+console.log(
+    attacker,
+    getAttackScore(attacker)
+);
