fc3ad89f6a957d1ef0766e810bd1cd957fd973e6
Guillaume Belleuvre <guillaumebelleuvre@gmail.com>
2024-04-10 15:15:55 +0200

    create characters data in an array

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
-console.log("FIGHTTTTTT!");
+const characters = [
+    {
+        name: 'Godzilla',
+        life: 50,
+        xp: 5,
+        weapon: 9,
+        shield: 4,
+        alive: true
+    },
+    {
+        name: 'Kong',
+        life: 50,
+        xp: 7,
+        weapon: 8,
+        shield: 7,
+        alive: true
+    },
+    {
+        name: 'Hulk',
+        life: 50,
+        xp: 4,
+        weapon: 3,
+        shield: 2,
+        alive: true
+    },
+    {
+        name: 'Aquaman',
+        life: 50,
+        xp: 4,
+        weapon: 3,
+        shield: 1,
+        alive: true
+    }
+];
+
+console.table(characters);
