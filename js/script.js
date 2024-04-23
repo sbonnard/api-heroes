3cab8f38722ab05a2f98083bbb0ba2bcee3f6538
caravane <thomasdesse.pro@gmail.com>
2024-04-23 12:16:01 +0200

    add data on challenger

--- js/script.js
+++ js/script.js
@@ -1,42 +1,28 @@
+import  Heroes from "./functions.js"
 
-/**
- * Return a random value between 0 and a chosen number.
- * @param {number} max - chosen number
- * @returns {number} - random value
- */
-function getRandomValue(max) {
-    return Math.floor(Math.random() * (max + 1));
-}
-
-
-/**
- * Gets a random value from an array
- * @param {array} array - Array of datas 
- * @returns {*} - a random value
- */
-function getRandomArrayValue(array) {
-    return array[getRandomValue(array.length - 1)];
-}
-
-
+const ulChallengers = document.getElementById("selected-challengers");
 const ul = document.getElementById("heroes-lst");
 const template = document.getElementById('hero-template');
+let templateContent;
 
 async function waitingForResponse() {
     try {
-
+        
         const response = await fetch("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json");
-
+        
         const allHeroes = await response.json()
-        console.log(getChallengers(allHeroes));
-
-        getChallengers(allHeroes).forEach(hero => {
-
-            const arrayChallengers = [];
-
+        console.log(Heroes.getChallengers(allHeroes));
+        const arrayChallengers = [];
+        const selectedChallengers = [];
+        
+        Heroes.getChallengers(allHeroes).forEach(hero => {
+            
+            
             //contente of the template 
-            const templateContent = document.importNode(template.content, true);
+            templateContent = document.importNode(template.content, true);
 
+            templateContent.querySelector("[data-id]").setAttribute("data-id", hero.id)  
+ 
             templateContent.querySelector('.js-img').src = hero.images.sm;
 
             templateContent.querySelector('.js-name').textContent = hero.name;
@@ -47,15 +33,22 @@ async function waitingForResponse() {
 
             templateContent.querySelector('.js-defense').textContent = hero.powerstats.combat;
 
+            arrayChallengers.push(hero)
+
             ul.appendChild(templateContent);
 
-            //button to select characters
-            const choiceBtn = document.querySelector(".js-btn");
-            choiceBtn.addEventListener("click", function(event) {
-                
-            })
         });
+
+
+        document.querySelectorAll(".js-btn");
+        console.log(document.querySelectorAll(".js-btn"));
+        for ( const selectedHero of selectedChallengers) {
+            console.log(selectedHero);
+
+            
+        }
         // return getChallengers(allHeroes);
+        //button to select characters
 
     }
 
@@ -75,154 +68,36 @@ console.log(waitingForResponse());
 // console.log(getRandomArrayValue(allHeroes));
 
 
-let characters = [
-    {
-        name: 'Chun-li',
-        life: 50,
-        xp: 9,
-        weapon: 9,
-        shield: 4
-    },
-    {
-        name: 'Ryu',
-        life: 50,
-        xp: 7,
-        weapon: 8,
-        shield: 7
-    },
-    {
-        name: 'Ken',
-        life: 50,
-        xp: 7,
-        weapon: 7,
-        shield: 7
-    },
-    {
-        name: 'Zangief',
-        life: 50,
-        xp: 5,
-        weapon: 5,
-        shield: 2
-    }
-];
-
-/**
- * Get random attack score from character stats
- * @param {object} attacker - An object representing a character
- * @returns {number} - Random attack score
- */
-function getAttackScore(attacker) {
-    return getRandomValue(attacker.weapon) + attacker.xp;
-}
-
-/**
- * Get random defense score from character stats
- * @param {object} defender - An object representing a character
- * @returns {number} - Random defense score
- */
-function getDefenseScore(defender) {
-    return getRandomValue(defender.shield) + defender.xp;
-}
-
-/**
- * Get 2 different and random challengers for a fight.
- * @param {array} charactersList The array you want the character's to be from.
- * @returns {array} First index of the array will draw the attacker and the second one will draw the defender.
- */
-function getChallengers(charactersList) {
-    let challengers = [];
-    while (challengers.length < 10) {
-        const c = getRandomArrayValue(charactersList);
-        if (!challengers.includes(c)) {
-            challengers.push(c);
-        }
-    }
-    return challengers;
-}
-
-// console.log(getChallengers(allHeroes));
-
-/**
- * fight between two characters and define the winner and the loser.
- * @param {array} challengers the first element in the array is the attacker and the second is the defender. they are objects. 
- * @returns {string} A text to explain the fight.
- */
-function fight(challengers) {
-    const attacker = challengers[0];
-    const defender = challengers[1];
-
-    let txt = '';
-
-    const attackPoints = getAttackScore(attacker);
-    if (attackPoints > getDefenseScore(defender)) {
-        defender.life -= attackPoints;
-
-        txt += `${attacker.name} attaque ${defender.name} et a gagné le combat en lui infligeant ${attackPoints} points de dégats.`;
-
-        if (!isAlive(defender)) {
-            txt += ` ${defender.name} est mort 💀`
-        }
-    }
-    else {
-        txt += `${defender.name} a contré l'attaque de ${attacker.name}.`;
-    }
-
-    return txt;
-}
-
-/**
- * Test if a character is alive and return true if he is
- * @param {object} character -The character's object
- * @returns {boolean} -True if alive, false if dead
- */
-function isAlive(character) {
-    return character.life > 0;
-}
+// let characters = [
+//     {
+//         name: 'Chun-li',
+//         life: 50,
+//         xp: 9,
+//         weapon: 9,
+//         shield: 4
+//     },
+//     {
+//         name: 'Ryu',
+//         life: 50,
+//         xp: 7,
+//         weapon: 8,
+//         shield: 7
+//     },
+//     {
+//         name: 'Ken',
+//         life: 50,
+//         xp: 7,
+//         weapon: 7,
+//         shield: 7
+//     },
+//     {
+//         name: 'Zangief',
+//         life: 50,
+//         xp: 5,
+//         weapon: 5,
+//         shield: 2
+//     }
+// ];
 
 
-/**
- * Delete a character from is array if he is out of hp
- * @param {array} charactersArray -The characters array
- * @returns {array} -The new array without characters out of hp
- */
-function burnTheDead(charactersArray) {
-    return charactersArray.filter(isAlive);
-}
 
-/**
- * Fight until only 1 remains
- * @param {array} characterArray -The array with all our characters
- * @return {object} -The winner's object
- */
-function startBattleRoyal(characterArray) {
-    while (characterArray.length > 1) {
-        const challengers = getChallengers(characterArray);
-        console.log(fight(challengers));
-        characterArray = burnTheDead(characterArray);
-    }
-    return characterArray[0];
-}
-
-function startBattleRoyalRec(characterArray) {
-    if (characterArray.length === 1) return characterArray[0];
-
-    console.log(fight(getChallengers(characterArray)));
-    return startBattleRoyalRec(burnTheDead(characterArray));
-}
-
-function startBattleRoyalInterval(characterArray) {
-
-    const timer = setInterval(() => {
-        const challengers = getChallengers(characterArray);
-        console.log(fight(challengers));
-        characterArray = burnTheDead(characterArray);
-
-        if (characterArray.length === 1) {
-            clearInterval(timer);
-            console.table(characterArray[0]);
-        }
-    }, 1000);
-}
-
-// startBattleRoyalInterval(characters);
-// console.table(startBattleRoyalInterval(characters));
